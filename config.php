09fb7453681c086b3b672be85dc478eed7fd6e55
Enamul Haque <shuvoworld@gmail.com>
2013-02-18 17:28:45 +0600

    Update config.php
    to make work date and hour difference function update timezone setting

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ERROR | E_WARNING | E_PARSE );
 session_start();
-
+@date_default_timezone_set ('Europe/Berlin');
 include_once('inc.globalvariables.php');
 include_once('inc.functions.generic.php');
 include_once('inc.functions.appspecific.php');
