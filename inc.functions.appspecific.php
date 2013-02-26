dc43d3f825bb21e6d6ce32057466aca5d91d8b26
shuvodu52 <shuvoworld@gmail.com>
2013-02-26 15:01:51 +0600

    addnotification to autoroaster

--- inc.functions.appspecific.php
+++ inc.functions.appspecific.php
@@ -434,6 +434,7 @@ function emailCurrentFullShiftDetails($xDays){
 					$Body.= "<a href='$scriptpath/security_assignment_add.php?sa_id=".$arr[$i][sa_id]."&param=edit' class='none'>Edit</a> | ";
 		
 					$Body.= "<a href='$scriptpath/security_assignment_add.php?sa_id=".$arr[$i][sa_id]."&param=view'>View</a>";
+					$Body.= "<a href='$scriptpath/snippets/security_assignment/ajax_notify.php?sa_id=".$arr[$i][sa_id].">Notify</a>";
 				$Body.= "</span></td></tr>";
 			}
 			$Body.="</table>";
