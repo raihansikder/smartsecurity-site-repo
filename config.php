e2bf1c155b3fbf639855776e4001d4400d210ec9
Enamul Haque <shuvoworld@gmail.com>
2013-03-24 02:18:40 +0600

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 error_reporting(E_ERROR | E_WARNING | E_PARSE );
 session_start();
 define('TIMEZONE', 'America/New_York');
-date_default_timezone_set(TIMEZONE);
-SET time_zone="offset";
+date_default_timezone_set('TIMEZONE');
+SET time_zone='offset';
 
 $now = new \DateTime();
 $mins = $now->getOffset() / 60;
